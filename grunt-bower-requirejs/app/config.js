1a9631ace9290e0f1a7616ab299b981299297984
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2016-06-03 16:32:45 -0300

    Tests with grunt, bower and requirejs

---
+++ grunt-bower-requirejs/app/config.js
@@ -0,0 +1,12 @@
+requirejs.config({
+  shim: {
+
+  },
+  paths: {
+    jquery: "../bower_components/jquery/dist/jquery",
+    "jquery-1.12.4": "../bower_components/jquery-1.12.4.min/index"
+  },
+  packages: [
+
+  ]
+});
